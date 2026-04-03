614f8263138d94ee15acf900c9bba4c1b7a01199
Alvy Azhari Aulia <alvyazhariaulia@gmail.com>
2026-04-03 13:01:02 +0700

    Implement login functionality in MainActivity

---
+++ MainActivity.kt
@@ -0,0 +1,41 @@
+package com.utama.aplikasiloginsederhana
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+class MainActivity : AppCompatActivity() {
+    private lateinit var etUsername: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnLogin: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        // Inisialisasi komponen
+        etUsername = findViewById(R.id.etUsername)
+        etPassword = findViewById(R.id.etPassword)
+        btnLogin = findViewById(R.id.btnLogin)
+        // Aksi saat tombol login diklik
+        btnLogin.setOnClickListener {
+            val username = etUsername.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+                // Validasi sederhana
+                if (username.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(this, "Username dan Password tidak boleh kosong!",
+                        Toast.LENGTH_SHORT).show()
+                } else {
+                    // Jika sukses, akan menampilkan pesan sukses
+                    Toast.makeText(this, "Anda berhasil login!",
+                        Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, HomeActivity::class.java)
+                    intent.putExtra("username", username)
+                    startActivity(intent)
+                }
+        }
+    }
+}
